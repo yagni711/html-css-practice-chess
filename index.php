6f3eaf8f64c83adeaa4bb1bead8971fb82259fb4
Yagni Patel <140079410+yagni711@users.noreply.github.com>
2023-07-30 10:43:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Chess</title>
+<link rel="icon" type="image/x-icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt4dirYFZGmEqJdbzkel2FcVL-YrjEvK8Cvg&usqp=CAU">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <div class="container">
+ <!----------------------------------------- first div start ----------------------------------------------------->
+        <div class="odd_div">
+        <div><a>♜</a></div>
+        <div><a>♞</a></div>
+        <div><a>♝</a></div>
+        <div><a>♛</a></div>
+        <div><a>♚</a></div>
+        <div><a>♝</a></div>
+        <div><a>♞</a></div>
+        <div><a>♜</a></div>
+        </div>
+        
+        <div class="even_div">
+      
+        <div><a>♟</a></div>
+        <div><a>♟</a></div>
+        <div><a>♟</a></div>
+        <div><a>♟</a></div> 
+        <div><a>♟</a></div>
+        <div><a>♟</a></div>
+        <div><a>♟</a></div>
+        <div><a>♟</a></div>
+     
+        </div>
+
+        <div class="odd_div">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+        
+        <div class="even_div">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div> 
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+
+        <div class="odd_div">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+        
+        <div class="even_div">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div> 
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+
+        <div class="odd_div">
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        <div><a>♙</a></div>
+        </div>
+        
+        <div class="even_div">
+        <div><a>♖</a></div>
+        <div><a>♘</a></div>
+        <div><a>♗</a></div>
+        <div><a>♔</a></div> 
+        <div><a>♕</a></div>
+        <div><a>♗</a></div>
+        <div><a>♘</a></div>
+        <div><a>♖</a></div>
+        </div>
+
+      
+ <!----------------------------------------- first div end -------------------------------------------------------->
+</div>
+</body>
+</html>
+
